2b45e322ffe1e051047c4fa84bfca76700673697
John Torres <jtorres0802@gmail.com>
2019-08-16 12:41:45 -0400

    Courses dropdown, courses table, and details page functional.

--- public/scripts/courses.js
+++ public/scripts/courses.js
@@ -1,1 +1,48 @@
-"use strict"
+"use strict";
+
+$(function() {
+  // Populates dropdown
+  let categorySelector = $("#categorySelector");
+
+  $.getJSON("/api/categories", function(data) {
+    let categories = data;
+
+    for (let i = 0; i < categories.length; i++) {
+      let catsAll = categories[i];
+      let element = document.createElement("option");
+      element.text = catsAll.Category;
+      element.value = catsAll.Value;
+      categorySelector.append(element);
+    }
+  });
+
+  categorySelector.on("change", function() {
+
+  $.getJSON("/api/courses/bycategory/" + $("#categorySelector").val(), function(data) {
+    let courses = data;
+      let tableBody = $("#courseTable");
+      tableBody.empty();
+
+      for (let i = 0; i < courses.length; i++) {
+          let courseId = courses[i].CourseId;
+          let courseTitle = courses[i].Title;
+          let courseLoc = courses[i].Location;
+
+          let markup =
+            "<tr><td>" +
+            courseId +
+            "</td><td>" +
+            courseTitle +
+            "</td><td>" +
+            courseLoc +
+            "</td><td>" +
+            "<a href='details.html?courseid=" +
+            courseId +
+            "'>Details</a></td></tr>";
+
+          tableBody.append(markup);
+      }
+      
+    });
+  });
+});
--- public/scripts/details.js
+++ public/scripts/details.js
@@ -1,1 +1,44 @@
-"use strict"
+"use strict";
+
+$(function() {
+  let urlParams = new URLSearchParams(location.search);
+  let courseId = urlParams.get("courseid");
+  let courseTable = $("#courseTable");
+
+  $.getJSON("/api/courses/" + courseId, function(data) {
+    let courses = data;
+
+    // display selected products from category.
+
+        let classId = courses.CourseId;
+        let classTitle = courses.Title;
+        let classCat = courses.Category;
+        let classLoc = courses.Location;
+        let classStart = courses.StartDate;
+        let classEnd = courses.EndDate;
+        let classMeets = courses.Meets;
+        let classFee = courses.Fee;
+
+        let markup =
+        "<tr><td>" +
+        classId +
+        "</td><td>" +
+        classTitle +
+        "</td><td>" +
+        classCat +
+        "</td><td>" +
+        classLoc +
+        "</td><td>" +
+        classStart +
+        "</td><td>" +
+        classEnd +
+        "</td><td>" + 
+        classMeets + 
+        "</td><td>" + 
+        classFee +
+        "</td></tr>";
+
+      courseTable.append(markup);
+      
+    })
+  });
