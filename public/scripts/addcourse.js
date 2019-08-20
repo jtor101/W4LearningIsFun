d2aff3bc9bd228e17a8ee4cf2c421f214e6d02ea
John Torres <jtorres0802@gmail.com>
2019-08-20 14:40:10 -0400

    Add Course validation is functional.

--- public/scripts/addcourse.js
+++ public/scripts/addcourse.js
@@ -16,11 +16,79 @@ $(function() {
   // Submit Button sends form.
   $("#caSubmitBtn").on("click", sendForm);
 
+  // Input Validation
+  function inputVal() {
+    $("form div div").empty();
+
+    if (
+      $("#courseid")
+        .val()
+        .trim() == ""
+    ) {
+      $("#courseiderror").text("Course ID required");
+      return false;
+    }
+    if (
+      $("#coursetitle")
+        .val()
+        .trim() == ""
+    ) {
+      $("#coursetitleerror").text("Course Title required");
+      return false;
+    }
+    if (
+      $("#courselocation")
+        .val()
+        .trim() == ""
+    ) {
+      $("#courselocationerror").text("Course Location required");
+      return false;
+    }
+    if (
+      $("#coursestartdt")
+        .val()
+        .trim() == ""
+    ) {
+      $("#coursestartdterror").text("Start Date required");
+      return false;
+    }
+    if (
+      $("#courseenddt")
+        .val()
+        .trim() == ""
+    ) {
+      $("#courseenddterror").text("End Date required");
+      return false;
+    }
+    if (
+      $("#coursemeets")
+        .val()
+        .trim() == ""
+    ) {
+      $("#coursemeetserror").text("Meeting Time required");
+      return false;
+    }
+    if (
+      $("#coursefee")
+        .val()
+        .trim() == ""
+    ) {
+      $("#coursefeeerror").text("Course Fee required");
+      return false;
+    }
+  }
+
   // Posts to Students table.
   function sendForm() {
-    alert("Sent!");
-    $.post("api/courses", $("#courseAdd").serialize());
-    window.location.replace("details.html?courseid=" + $("#courseid").val());
-    return false;
+    $("#errors").empty();
+    let isOk = inputVal();
+    if (isOk == false) {
+      return;
+    } else {
+      alert("Sent!");
+      $.post("api/courses", $("#courseAdd").serialize());
+      window.location.href = "courses.html"
+      return false;
+    }
   }
 });
